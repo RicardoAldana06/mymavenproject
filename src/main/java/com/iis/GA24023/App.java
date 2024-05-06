1f00c425b3f8836e0b5c5e460fcdd2ac1fe56160
RicardoAldana <ricardoaldana827@gmail.com>
2024-05-05 22:42:26 -0600

    agregando el proyecto

---
+++ src/main/java/com/iis/GA24023/App.java
@@ -0,0 +1,18 @@
+package com.iis.GA24023;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+    public String Hello() {
+    	return "Hello world";
+    }
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+    	System.out.println(hw.Hello() );
+    }
+}
---
+++ src/test/java/com/iis/GA24023/AppTest.java
@@ -0,0 +1,21 @@
+package com.iis.GA24023;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    
+    }
+}
